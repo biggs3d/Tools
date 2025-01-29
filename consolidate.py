1f0300063044cd266c5ffe238d1096482da8147e
Steve <biggs3d@gmail.com>
2025-01-29 10:01:28 -0600

    changed default to not create separate files for child directories

--- consolidate.py
+++ consolidate.py
@@ -37,29 +37,8 @@ Notes:
 - The script creates a new file with a timestamp in its name to avoid overwriting existing files.
 
 Author: Steve Biggs
-Last Modified: 2024.07.12
-Version: 2.1
-
-License: MIT, Copyright (c) 2024, Steve Biggs
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
+Last Modified: 2025.01.29
+Version: 2.2
 """
 
 
@@ -164,7 +143,7 @@ class FileHandler:
     def ask_user() -> bool:
         response = input(f"Do you want to run the script just on the child directories (separate output)? "
                          f"[y/n] (default: y): ")
-        return response.lower() in ["", "y", "yes"]
+        return response.lower() in ["y", "yes"]  # Default to 'n'/false if the input is empty
 
     @staticmethod
     def combine_files_with_path_headers(folder: str, files_by_path: dict[str, str],
